730c8915b96a4186aae631562906089fc05ca4a2
AzeemMuzammil <azeemmuzammil@yahoo.com>
2019-11-20 01:13:41 +0530

    New page added

---
+++ lib/diary.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Diary extends StatefulWidget {
+  @override
+  _Diary createState() => _Diary();
+}
+
+class _Diary extends State<Diary> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+    );
+  }
+}
