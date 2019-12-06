3fc35a05709af5cf9cbbffdb7b9f7ca33da57eda
AzeemMuzammil <azeemmuzammil@yahoo.com>
2019-12-06 19:46:35 +0530

    delete

--- lib/models/posts.dart
+++
@@ -1,9 +0,0 @@
-import 'package:ui_design/models/users.dart';
-
-class Posts {
-  Users user;
-  String title;
-  String description;
-
-  Posts({this.user, this.title, this.description});
-}
--- lib/models/users.dart
+++
@@ -1,5 +0,0 @@
-class Users {
-  String uid;
-  String name;
-  Users({this.name, this.uid});
-}
