45e98fe55d52893d3b0aa7345a9e364da099b96c
dilini <linifarnando123@gmail.com>
2020-02-03 12:10:07 +0530

    Added firebase

--- lib/ui/home_page_bloc.dart
+++ lib/ui/home_page_bloc.dart
@@ -10,7 +10,9 @@ import 'home_page_model.dart';
 class HomePageBloc extends Bloc<HomePageAction, HomePageModel> {
   static final log = Log("HomePageBloc");
 
-  HomePageBloc(BuildContext context);
+  HomePageBloc(BuildContext context){
+    add(LoadAllCardAction());
+  }
 
   @override
   HomePageModel get initialState => HomePageModel(
@@ -33,6 +35,26 @@ class HomePageBloc extends Bloc<HomePageAction, HomePageModel> {
         yield state.clone(titles: [],descriptions: []);
         yield state.clone(titles: titles, descriptions: descriptions);
         break;
+
+      case LoadAllCardAction:
+        final posts = await postRepo.querySingle(specification: ComplexSpecification([]));
+        for (var list in posts) {
+          final titles = List.of(state.titles);
+          titles.add(list.title);
+
+          final descriptions = List.of(state.descriptions);
+          descriptions.add(list.description);
+
+          yield state.clone(titles: titles, descriptions: descriptions);
+        }
+
+        break;
     }
+  }
+
+  @override
+  void onError(Object error, StackTrace stacktrace) {
+    log.e('$error');
+    log.e('$stacktrace');
   }
 }
